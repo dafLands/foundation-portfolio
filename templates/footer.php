f64ac06ae26248f320a6dcac965f1e1edd50c023
Jason Garner <jason.m.garner@gmail.com>
2013-06-07 09:53:21 -0500

    Added SVG pattern to jumbotron

--- templates/footer.php
+++ templates/footer.php
@@ -12,5 +12,23 @@
 	<script>jQuery(document).ready(function($){if($(".content.portfolio").length){$portThumb=$(".portfolio-thumbnail");$screenieWrap=$(".screenshot-wrap");$screenieWrap.addClass("active");$(".portfolio-thumbnail a").on("mouseenter click",function(e){var $this=$(this),$screenShot=$("img.screen-shot"),$currImage=$screenShot.attr("src"),$newImage=$this.attr("data-image");$portThumb.removeClass("active");$screenieWrap.removeClass("active");$this.parent("li").addClass("active");$("img.screen-shot").attr("src",
 	$newImage).delay(100).parent("span").addClass("active");return false})}});</script>
 <?php endif ?>
+<?php if (is_front_page()): ?>
+	<script>
+		// Short script to encode our SVG in base64
+		// This can be reversed using window.atob('base64')
+		var svg = document.getElementsByTagName('svg')[0];
+
+		// Convert the SVG node to HTML.
+		var div = document.createElement('div');
+		div.appendChild(svg.cloneNode(true));
+
+		// Encode the SVG as base64
+		var b64 = 'data:image/svg+xml;base64,'+window.btoa(div.innerHTML);
+		var url = 'url("' + b64 + '")';
+
+		codeCss = 'background-image: ' + url + ';';
+		$('#jumbotron').attr('style', codeCss);
+	</script>
+<?php endif; ?>
 <?php wp_footer(); ?>
 <script>jQuery(document).foundation();</script>
