73eccc34bdf28af49cc7eda64e30d51f50ada8a7
Jason Garner <jason.m.garner@gmail.com>
2013-06-06 22:43:24 -0500

    Added column styles and made page template column layout

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,8 +1,8 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-		<?php the_title('<h1 class="page-title">', '</h1><hr />') ?>
-  <?php the_content(); ?>
+		<header><?php the_title('<h1 class="page-title">', '</h1><hr />') ?></header>
+  		<div class="entry-content content-columns"><?php the_content(); ?></div>
   </article>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
