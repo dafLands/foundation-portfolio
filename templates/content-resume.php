4353d68db9e1ad0310a93e1fb7cc0a0c23344a08
Jason Garner <jason.m.garner@gmail.com>
2013-06-11 16:10:17 -0500

    resume template markup
    removed custom fields in resume functions file.

--- templates/content-resume.php
+++ templates/content-resume.php
@@ -2,9 +2,40 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="large-4 columns">
   	<div class="intro"><?php the_content(); ?></div>
+  	<div class="skills"></div>
   </div>
   <div class="large-8 columns">
-  		<div class="intro"><?php the_content(); ?></div>
+  	<div class="experience">
+  		<?php $experience_values = simple_fields_fieldgroup('work_experience');
+  			foreach($experience_values as $values) {
+				echo '<h2>' .$values['title']. '</h2>';
+				echo '<h3>' .$values['name']. '</h3>';
+				echo '<h4>' .$values['location']. '</h4>';
+				echo '<p>' .$values['duties']. '</p>';
+				echo '<p class="date-range">';
+				$start = $values['start']['date_format'];
+				$end = $values['end']['date_format'];
+				$pattern = '([0-9][,]{1,2})';
+				echo '<p class="date-range">';
+				echo preg_replace($pattern, '', $start). ' - ';
+				if ($values['current'])
+					echo 'Present';
+				else
+					echo preg_replace($pattern, '', $end);
+				echo '</p>';
+	  		 }
+	  	?>
+  	</div>
+  	<div class="education">
+  		<?php
+  			$education_values = simple_fields_fieldgroup('education');
+  			foreach ($education_values as $values) {
+  				echo '<h2>' .$values['degree']. '</h2>';
+  				echo '<p class="grad-info">';
+  				echo $values['school']. ' - ' .$values['date'];
+  				echo '</p>';
+  			}
+  		 ?>
+  	</div>
   </div>
-
 <?php endwhile; ?>
