a192b2deef8a33c8debb4e63c31c34a4ba1b1697
Jason Garner <jason.m.garner@gmail.com>
2013-06-25 13:35:03 -0500

    added remove version function to custom functions

--- lib/custom.php
+++ lib/custom.php
@@ -106,3 +106,9 @@ function getMainMenu($menulocation){
       return true;
     }
 }
+
+// remove version info from head and feeds
+function complete_version_removal() {
+    return '';
+}
+add_filter('the_generator', 'complete_version_removal');
